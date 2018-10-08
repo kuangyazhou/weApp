93bb5ea414f7903526832791d551453c982833d2
况亚洲 <18707174713@139.com>
2018-10-08 16:40:39 +0800

    fuck taro-ui

--- src/pages/taroUI/index.js
+++ src/pages/taroUI/index.js
@@ -1,12 +1,46 @@
 import Taro, { Component } from "@tarojs/taro";
 import { View } from "@tarojs/components";
-import { AtButton } from "taro-ui";
+import {
+  AtButton,
+  AtModal,
+  AtModalHeader,
+  AtModalContent,
+  AtModalAction,
+  AtTabs,
+  AtTabsPane,
+  AtTabBar
+} from "taro-ui";
 
 export default class TaroUI extends Component {
   config = {
     navigationBarTitleText: "TaroUI"
   };
+  state = {
+    isOpened: true,
+    current: 0,
+    barCur: 0
+  };
 
+  cancel = e => {
+    // console.log(e);
+    this.setState({ isOpened: false });
+  };
+
+  confirm = e => {
+    // console.log(e);
+    this.setState({ isOpened: true });
+  };
+  handleClick = e => {
+    this.setState({
+      current: e
+    });
+  };
+
+  barClick = e => {
+    this.setState({
+      barCur: e
+    });
+  };
   test = e => {
     console.log(e + "fuck the king!!!");
   };
@@ -18,6 +52,62 @@ export default class TaroUI extends Component {
         <AtButton type="secondary" onClick={this.test}>
           按钮文案
         </AtButton>
+        <AtButton onClick={this.confirm}>显示Modal</AtButton>
+        <AtTabs
+          current={this.state.current}
+          tabList={[
+            { title: "标签页1" },
+            { title: "标签页2" },
+            { title: "标签页3" }
+          ]}
+          onClick={this.handleClick}
+        >
+          <AtTabsPane current={this.state.current} index={0}>
+            <View style="font-size:18px;text-align:center;height:100px;">
+              标签页一的内容
+            </View>
+          </AtTabsPane>
+          <AtTabsPane current={this.state.current} index={1}>
+            <View style="font-size:18px;text-align:center;height:100px;">
+              标签页二的内容
+            </View>
+          </AtTabsPane>
+          <AtTabsPane current={this.state.current} index={2}>
+            <View style="font-size:18px;text-align:center;height:100px;">
+              标签页三的内容
+            </View>
+          </AtTabsPane>
+        </AtTabs>
+        {/* <AtModal isOpened onCancel={this.cancel} onConfirm={this.confirm}>
+          <AtModalHeader>标题</AtModalHeader>
+          <AtModalContent>
+            这里是正文内容，欢迎加入京东凹凸实验室
+            这里是正文内容，欢迎加入京东凹凸实验室
+            这里是正文内容，欢迎加入京东凹凸实验室
+          </AtModalContent>
+          <AtModalAction>
+            <Button>取消</Button>
+            <Button>确定</Button>
+          </AtModalAction>
+        </AtModal> */}
+        {/* <AtTabBar
+          tabList={[
+            { title: "待办事项", text: 8 },
+            { title: "拍照" },
+            { title: "通讯录", dot: true }
+          ]}
+          onClick={this.barClick}
+          current={this.state.barCur}
+        /> */}
+        <AtModal
+          isOpened={this.state.isOpened}
+          title="标题"
+          cancelText="取消"
+          confirmText="确认"
+          onCancel={this.cancel}
+          onConfirm={this.cancel}
+          content="欢迎加入京东凹凸实验室\n\r欢迎加入京东凹凸实验室"
+        />
       </View>
     );
   }
